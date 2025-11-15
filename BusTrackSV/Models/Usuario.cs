746b03b93c8e441b16e120ccbf96fcea06b0f3d7
Orlen-07 <adonayrafaelano17@gmail.com>
2025-11-14 22:32:07 -0600

    Eliminación de Usuario_validado de acá

--- BusTrackSV/Models/Usuario.cs
+++ BusTrackSV/Models/Usuario.cs
@@ -8,11 +8,4 @@ namespace BusTrackSV.Models
         public string usuario { get; set; }
         public string password {get; set; }
     }
-    public class Usuario_validado
-    {
-        public int id_usuario { get; set; }
-        public string nombre_completo { get; set; }
-        public string correo { get; set; }
-        public string usuario { get; set; }
-    }
 }
