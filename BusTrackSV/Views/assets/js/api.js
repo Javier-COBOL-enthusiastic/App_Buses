efe61664e8d7dc3a9bc8516ac0014f8e70f2b817
Javier-COBOL-enthusiastic <javier.moran@keyinstitute.edu.sv>
2025-12-03 08:39:06 -0600

    arreglando cosas y funciones

--- BusTrackSV/Views/assets/js/api.js
+++ BusTrackSV/Views/assets/js/api.js
@@ -540,3 +540,22 @@ export const api = {
     });
   },
 };
+
+function HaversineDistance(lat1, lng1, lat2, lng2)
+  {    
+    const R = 6371000; // Radio de la Tierra en metros
+    const toRad = x => x * Math.PI / 180;
+
+    const dLat = toRad(lat2 - lat1);
+    const dLng = toRad(lng2 - lng1);
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    
+    return R * c; // Distancia en metros
+  }
--- BusTrackSV/Views/assets/js/dashboard.js
+++ BusTrackSV/Views/assets/js/dashboard.js
@@ -1,6 +1,6 @@
   import { renderHeader, renderFooter } from "./components.js";
   import { openModal, closeModal, toast } from "./ui.js";
-  import { api } from "./api.js";
+  import { api, HaversineDistance } from "./api.js";
   import { storage } from "./storage.js";
 
   // ───────────────────────────────────
@@ -260,7 +260,7 @@
           if(distance > 300) {
             toast
             (
-            `Error: El punto está a ${Math.round(distance)}m del anterior. Máximo permitido: 500m.`,
+            `Error: El punto está a ${Math.round(distance)}m del anterior. Máximo permitido: 300m.`,
             "bad"
             );
             return;
@@ -394,21 +394,4 @@
   setToolbarState();
 
 
-  function HaversineDistance(lat1, lng1, lat2, lng2)
-  {    
-    const R = 6371000; // Radio de la Tierra en metros
-    const toRad = x => x * Math.PI / 180;
-
-    const dLat = toRad(lat2 - lat1);
-    const dLng = toRad(lng2 - lng1);
-
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-        Math.sin(dLng / 2) * Math.sin(dLng / 2);
-
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    
-    return R * c; // Distancia en metros
-  }
+  
